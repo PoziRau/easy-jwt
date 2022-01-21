7f9f2be0852f1d9f54bed1c0d4312f350192256d
PoziRau <babushkin.ian@gmail.com>
2022-01-20 22:20:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const crypto = require("crypto");
 
 exports.token = (json, secret) => {
     header = { alg: "HS256", typ: "JWT"}
-    return header.toString('base64') + '.' + json.toString('base64') + '.' + crypto.createHmac('sha256', secret).update(header + json).digest("base64");
+    return btoa(JSON.stringify(header)) + '.' + btoa(JSON.stringify(json)) + '.' + crypto.createHmac('sha256', secret).update(header + json).digest("base64");
 }
 
 exports.verify = (data, secret) => {
     var data = data.split('.') 
 
-    if(secret == null) { return data[1].toString('ascii') (Error ('No secret provided, verification not complete')) }
+    if (secret == null) { return atob(data[1]) (Error ('No secret provided, verification not complete')) }
 
-    if (data[2] === crypto.createHmac('sha256', secret).update(data[0] + data[1]).digest("base64")) {
+    if (data[2] === crypto.createHmac('sha256', secret).update(atob(data[0]) + atob(data[1])).digest("base64")) {
         return data[1].toString('ascii')
     } else {
-        return data (Error ('Failed verification'))
+        throw new Error('Failed verification')
     }
 }
-  
-exports.allColors = allColors;
